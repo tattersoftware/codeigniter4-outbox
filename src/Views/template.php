f39892c6b525f82ca3e603b980131d5c22d549eb
MGatner <mgatner@icloud.com>
2020-11-03 00:26:34 +0000

    Bugfix template variable

--- src/Views/template.php
+++ src/Views/template.php
@@ -115,7 +115,9 @@
                 <td class="wrapper">
                   <table role="presentation" border="0" cellpadding="0" cellspacing="0">
                     <tr>
-<?= $main ?? '' ?>
+
+<?= $body ?? '' ?>
+
                     </tr>
                   </table>
                 </td>
